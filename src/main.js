a58b3b42b9cee875adaf9fd9ff99a63a134e6466
Aaron Fischer <aaron.fischer@marbis.net>
2015-08-25 16:04:47 +0200

    Add more resources

--- src/main.js
+++ src/main.js
@@ -1,3 +1,5 @@
+// Theme: Reversed
+
 class Engine {
     constructor() {
         this.ticks = 0;
@@ -5,7 +7,9 @@ class Engine {
         this.asteroids = [this.current_asteroid];
         this.resources = {
             dust: 0,
-            crystals: 0
+            stone: 0,
+            carbon: 0,
+            metal: 0
         }
     }
 
@@ -15,8 +19,12 @@ class Engine {
 
     update() {
         document.querySelector('#time').innerHTML = this.ticks + ' hours';
+        document.querySelector('#asteroid-classification').innerHTML = this.current_asteroid.classification;
+
         document.querySelector('#res-dust').innerHTML = this.resources.dust;
-        document.querySelector('#res-crystals').innerHTML = this.resources.crystals;
+        document.querySelector('#res-stone').innerHTML = this.resources.stone;
+        document.querySelector('#res-carbon').innerHTML = this.resources.carbon;
+        document.querySelector('#res-metal').innerHTML = this.resources.metal;
     }
 }
 
@@ -32,8 +40,24 @@ class Player {
 }
 
 class Asteroid {
+    constructor() {
+        // Determine the class
+        this.classification = [
+            'A', // small, stone, dust, random
+            'C', // dark carbon
+            'S', // stone
+            'X' // metal
+        ][Helper.random_number(0, 4)];
+    }
+
     mine_resource() {
-        return ['dust', 'dust', 'dust', 'crystals'][Helper.random_number(0, 4)];
+        let resources = {
+            'A': ['dust', 'dust', 'stone'],
+            'C': ['dust', 'carbon', 'carbon', 'carbon'],
+            'S': ['dust', 'stone', 'stone'],
+            'X': ['metal']
+        }[this.classification];
+        return resources[Helper.random_number(0, resources.length)];
     }
 }
 
@@ -50,4 +74,6 @@ var bootstrap = function() {
         player.mine();   
         engine.update();
     };
+
+    engine.update();
 };
